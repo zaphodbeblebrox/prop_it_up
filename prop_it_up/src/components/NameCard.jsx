4dd068670d7bd5a2fff9f9f0856c9df564c6a218
Brandon Withrow <withrobm@gmail.com>
2023-06-14 22:25:07 -0700

    Complete Putting It Together Assignment
    in NameCard.jsx, a useState was created to track the personnel's age. A button was added, that when clicked, would increment the given personnel's age by 1.

--- prop_it_up/src/components/NameCard.jsx
+++ prop_it_up/src/components/NameCard.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 const NameCard = (props) => {
     const {personelInfo} = props;
+    const[age, setAge] = useState(personelInfo.age);
+    
+    const incrementAge = () => {
+        setAge(age + 1);
+        console.log(age);
+    }
     return (
         <div>
             <h2>{personelInfo.lastName}, {personelInfo.firstName}</h2>
-            <p>Age: {personelInfo.age}</p>
+            <p>Age: {age}</p>
             <p>Hair Color: {personelInfo.hairColor}</p>
+            <button onClick={incrementAge}>Birthday Button for {personelInfo.firstName} {personelInfo.lastName}</button>
         </div>
     );
 
